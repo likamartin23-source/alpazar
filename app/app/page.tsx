73e33ad10e01b4241750c7cf706278a1b4dec349
likamartin23-source <likamartin23@gmail.com>
2026-05-19 20:34:45 +0200

    Update and rename app/app/page.tsx to app/page.tsx

--- app/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function RootLayout({ children }: any) {
-  return (
-    <html>
-      <body>{children}</body>
-    </html>
-  );
-}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Alpazar Marketplace 🚀</h1>
+      <p>Platforma jote është LIVE ✅</p>
+    </div>
+  );
+}
